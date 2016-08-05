e389cbaec6ad87705803e77312a5f497c727d354
Alena <liolik77@yahoo.com>
2016-08-05 11:40:39 -0400

    1st version, homework section working

--- schoolboard/client/main.js
+++ schoolboard/client/main.js
@@ -3,20 +3,64 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.homework_list.helpers({
+	homework: function(){
+		return Homework.find({});
+	}
+});
+Template.homework_item.helpers({
+	homework: function(){
+		return Homework.find({});
+	},
+	modal_id: function(){
+		var homework_id = this._id;
+	   	var homework = Homework.findOne(homework_id);
+	   	console.log("id is");
+	   	console.log(homework_id);
+	   	return homework_id;
+	}
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+/////
+	// template events 
+/////
+
+Template.homework_item.events({
+	"click .js-modal":function(event){
+		console.log("modal clicked");
+		var modal_id = this._id;
+		console.log(modal_id);
+		$('#'+ modal_id).openModal();
+	} 
+})
+
+Template.homework_form.events({
+		"submit .js-submit-homework":function(event){
+
+			var due_date = event.target.due_date.value;
+			var homework_title = event.target.homework_title.value;
+			var description = event.target.description.value;
+			var comments = event.target.comments.value;
+			var subject = event.target.subject.value;
+			// var user = Meteor.user();
+			Homework.insert({
+				due_date: due_date,
+				homework_title: homework_title,
+				description: description,
+				comments: comments,
+				subject:  subject
+				});
+			console.log("inserted");
+					
+						
+		}
+	});
+
+
+$(document).ready(function(){
+	$(".button-collapse").sideNav(
+		);
+	$('.modal-trigger').leanModal();
+	$('select').material_select();
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
--- schoolboard/server/main.js
+++ schoolboard/server/main.js
@@ -1,5 +1,29 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  if (!Homework.findOne()){
+    	console.log("No homework yet yet. Creating starter data.");
+    	  Homework.insert({
+    		homework_title:"First homework to do", 
+    		due_date:"12/12/16", 
+    		subject: "math",
+    		description: "page 3-5",
+    		comments: "work hard!"
+    		 });
+    	Homework.insert({
+    		homework_title:"Second homework to do", 
+    		due_date:"12/13/16", 
+    		subject: "Social study",
+    		description: "page 3-5",
+    		comments: "work harder!"
+    		 });
+    	Homework.insert({
+    		homework_title:"Third homework to do", 
+    		due_date:"12/14/16", 
+    		subject: "English Language Arts",
+    		description: "page 3-5",
+    		comments: "Harder! Harder!"
+    		 });
+    }
+    console.log(Homework.findOne());
 });
